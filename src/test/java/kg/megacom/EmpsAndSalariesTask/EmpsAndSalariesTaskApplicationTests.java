8dc370cc675384d1b9e2ff0c7863310f1f2aeb4d
Nurzhan <nurzhankydyralieva@gmail.com>
2021-08-10 23:30:27 +0600

    Initial commit

---
+++ src/test/java/kg/megacom/EmpsAndSalariesTask/EmpsAndSalariesTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.megacom.EmpsAndSalariesTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpsAndSalariesTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
