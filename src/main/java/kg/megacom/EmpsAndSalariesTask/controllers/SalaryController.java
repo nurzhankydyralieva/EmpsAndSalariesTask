453ee49c0312ae20c3705540670b32f864a47390
Nurzhan <nurzhankydyralieva@gmail.com>
2021-08-10 23:41:45 +0600

    Created get mapper

--- src/main/java/kg/megacom/EmpsAndSalariesTask/controllers/SalaryController.java
+++ src/main/java/kg/megacom/EmpsAndSalariesTask/controllers/SalaryController.java
@@ -5,6 +5,8 @@ import kg.megacom.EmpsAndSalariesTask.services.SalaryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/salary")
 public class SalaryController {
@@ -20,5 +22,14 @@ public class SalaryController {
     @PutMapping("/{salaryId}")
     public SalaryDto updateSalary(@PathVariable("salaryId") Long salaryId, @RequestBody SalaryDto salaryDto) {
         return salaryService.updateSalary(salaryId, salaryDto);
+    }
+    @GetMapping
+    public List<SalaryDto> getSalaryAll() {
+        return salaryService.getSalaries();
+    }
+
+    @GetMapping("/{salaryId}")
+    public SalaryDto getSalaryById(@PathVariable("salaryId") Long salaryId) {
+        return salaryService.getSalary(salaryId);
     }
 }
